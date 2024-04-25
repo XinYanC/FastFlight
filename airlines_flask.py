6734c52ef70e4175f4c1109ec9a762df291f3655
XinYan <103906642+XinYanC@users.noreply.github.com>
2024-04-25 15:57:09 +0800

    login authen

--- airlines_flask.py
+++ airlines_flask.py
@@ -38,35 +38,67 @@ def bookingAgentRegistration():
 def staffRegistration():
 	return render_template('staffRegistration.html')
 
+@app.route('/customerInterface')
+def customerInterface():
+	return render_template('customerInterface.html')
+
+@app.route('/bookingAgentInterface')
+def bookingAgentInterface():
+	return render_template('bookingAgentInterface.html')
+
+@app.route('/staffInterface')
+def staffInterface():
+	return render_template('staffInterface.html')
 
 
 #Authenticates the login
-@app.route('/loginAuth', methods=['GET', 'POST'])
-def loginAuth():
+@app.route('/loginSubmit', methods=['GET', 'POST'])
+def loginSubmit():
 	#grabs information from the forms
 	username = request.form['login_email']
 	password = request.form['login_password']
+	user = request.form.get('user_type')
 
 	#cursor used to send queries
 	cursor = conn.cursor()
-	#executes query
-	query = "SELECT * FROM user WHERE username = '{}' and password = '{}'"
-	cursor.execute(query.format(username, password))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
+
+	if (user == "customer"):
+		query = "SELECT * FROM customer WHERE cust_email = '{}' and c_password = '{}'"
+		cursor.execute(query.format(username, password))
+		data = cursor.fetchone()
+	elif (user == "booking_agent"):
+		query = "SELECT * FROM booking_agent WHERE booking_agent_email = '{}' and ba_password = '{}'"
+		cursor.execute(query.format(username, password))
+		data = cursor.fetchone()
+	elif (user == "airline_staff"):
+		query = "SELECT * FROM airline_staff WHERE username = '{}' and s_password = '{}'"
+		cursor.execute(query.format(username, password))
+		data = cursor.fetchone()
+	# #executes query
+	# query = "SELECT * FROM user WHERE username = '{}' and password = '{}'"
+	# cursor.execute(query.format(username, password))
+	# #stores the results in a variable
+	# data = cursor.fetchone()
+	# #use fetchall() if you are expecting more than 1 data row
+
+
 	cursor.close()
 	error = None
 	if(data):
 		#creates a session for the the user
 		#session is a built in
 		session['username'] = username
-		return redirect(url_for('home'))
+		if (user == "customer"):
+			return redirect(url_for('customerInterface'))
+		elif (user == "booking_agent"):
+			return redirect(url_for('bookingAgentInterface'))
+		elif (user == "airline_staff"):
+			return redirect(url_for('staffInterface'))
 	else:
 		#returns an error message to the html page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
-	
+		loginError = 'Invalid login or username'
+		return render_template('login.html', loginError=loginError)
+
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
 #debug = True -> you don't have to restart flask
