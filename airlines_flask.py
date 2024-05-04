35a34793ad156418da7619901e4c739d53fef49e
XinYan <103906642+XinYanC@users.noreply.github.com>
2024-05-05 01:02:03 +0800

    create new flights

--- airlines_flask.py
+++ airlines_flask.py
@@ -24,34 +24,34 @@ conn = pymysql.connect(host='localhost',
 #Define a route to hello function
 @app.route('/')
 def home():
-    if ('username' in session):
-        if 'customer' in session:
-            cursor = conn.cursor()
-            cursor.execute("SELECT c_name FROM customer WHERE cust_email = %s", session['username'])
-            c_name = cursor.fetchone()[0]
-            cursor.close()
+	if ('username' in session):
+		if 'customer' in session:
+			cursor = conn.cursor()
+			cursor.execute("SELECT c_name FROM customer WHERE cust_email = %s", session['username'])
+			c_name = cursor.fetchone()[0]
+			cursor.close()
 
-            # Split the full name into words
-            name_parts = c_name.split()
-            # Join all but the last word
-            first_name = ' '.join(name_parts[:-1])
-            
-            return redirect(url_for('customerInterface', cust_name=first_name))
-        elif 'booking_agent' in session:
-            cursor = conn.cursor()
-            cursor.execute("SELECT booking_agent_id FROM booking_agent WHERE booking_agent_email = %s", session['username'])
-            booking_agent_id = cursor.fetchone()[0]
-            cursor.close()
+			# Split the full name into words
+			name_parts = c_name.split()
+			# Join all but the last word
+			first_name = ' '.join(name_parts[:-1])
+			
+			return redirect(url_for('customerInterface', cust_name=first_name))
+		elif 'booking_agent' in session:
+			cursor = conn.cursor()
+			cursor.execute("SELECT booking_agent_id FROM booking_agent WHERE booking_agent_email = %s", session['username'])
+			booking_agent_id = cursor.fetchone()[0]
+			cursor.close()
 
-            return redirect(url_for('bookingAgentInterface', booking_agent_id=booking_agent_id))
-        elif 'airline_staff' in session:
-            cursor = conn.cursor()
-            cursor.execute("SELECT first_name FROM airline_staff WHERE username = %s", session['username'])
-            first_name = cursor.fetchone()[0]
-            cursor.close()
+			return redirect(url_for('bookingAgentInterface', booking_agent_id=booking_agent_id))
+		elif 'airline_staff' in session:
+			cursor = conn.cursor()
+			cursor.execute("SELECT first_name FROM airline_staff WHERE username = %s", session['username'])
+			first_name = cursor.fetchone()[0]
+			cursor.close()
 
-            return redirect(url_for('staffInterface', staff_name=first_name))
-    return render_template('index.html')
+			return redirect(url_for('staffInterface', staff_name=first_name))
+	return render_template('index.html')
 
 
 @app.route('/login')
@@ -95,7 +95,11 @@ def bookingAgentInterface():
 @app.route('/staffInterface')
 def staffInterface():
 	staff_name = request.args.get('staff_name', 'again')  # Get staff_name from query parameter
-	return render_template('staffInterface.html', staff_name=staff_name)
+	staff_operator = False
+	if 'operator' in session:
+		staff_operator = True
+
+	return render_template('staffInterface.html', staff_name=staff_name, staff_operator=staff_operator)
 
 @app.route('/flightResult')
 def flightResult():
@@ -619,9 +623,22 @@ def loginSubmit():
 	# #stores the results in a variable
 	# data = cursor.fetchone()
 	# #use fetchall() if you are expecting more than 1 data row
-
-
 	cursor.close()
+
+	cursor = conn.cursor()
+	if 'airline_staff' in session:
+		query = "SELECT permission FROM permission WHERE username = %s"
+		cursor.execute(query, (username,))
+		staff_status = cursor.fetchall()
+		for status in staff_status:
+			if status[0] == 'admin':  # Assuming 'admin' is stored as a string in the database
+				session['admin'] = True
+				print('Admin:', session['admin'])
+			if status[0] == 'operator':  # Assuming 'operator' is stored as a string in the database
+				session['operator'] = True
+				print('Operator:', session['operator'])
+	cursor.close()
+
 	loginError = None
 
 	if(data):
@@ -1241,9 +1258,66 @@ def awards():
 @app.route('/createFlight')
 def createFlight():
 	if 'admin' in session:
-		return render_template('createFlight.html')
+		cursor = conn.cursor()
+		query = "SELECT MAX(flight_num) AS max_flight_num FROM flight"
+		cursor.execute(query)
+		result = cursor.fetchone()
+
+		if result and result[0] is not None:  # Check if result is not empty and the flight number is not None
+			max_flight_num = int(result[0])  # Extract the flight number from the result tuple and convert it to an integer
+			new_flight_num = max_flight_num + 1
+		else:
+			new_flight_num = 1
+
+		new_flight_num = str(new_flight_num).zfill(5)
+		cursor.close()
+
+
+		cursor = conn.cursor()
+		cursor.execute("SELECT airline_name FROM airline_staff WHERE username = %s", (session['username'],))
+		airline_name = cursor.fetchone()[0]
+		cursor.close()
+
+		cursor = conn.cursor()
+		cursor.execute("SELECT airport_name FROM airport ORDER BY airport_name")
+		airport_names = [row[0] for row in cursor.fetchall()]
+		cursor.close()
+
+		cursor = conn.cursor()
+		cursor.execute("SELECT airplane_id FROM airplane WHERE airline_name =%s", airline_name)
+		airplane_id = [row[0] for row in cursor.fetchall()]
+		cursor.close()
+
+		return render_template('createFlight.html', flight_num=new_flight_num, airline_name=airline_name, airport_names=airport_names, airplane_id=airplane_id)
 	else:
 		return render_template('viewFlights.html')
+	
+@app.route('/inputFlightInfo', methods=['GET', 'POST'])
+def inputFlightInfo():
+	# flight_num = request.args.get('flight_num')
+	# print('showwwwwww',flight_num)
+	# airline_name = request.args.get('airline_name')
+	# flight_num = request.form['flight_num']
+	flight_num = request.form.get('flight_num')
+	print('showwwwwww',flight_num)
+	airline_name = request.form.get('airline_name')
+	# airline_name = request.form['airline_name']
+	depart_airport_name = request.form['depart_airport_name']
+	arrival_airport_name = request.form['arrival_airport_name']
+	departure_time = request.form['departure_time']
+	arrival_time = request.form['arrival_time']
+	price = request.form['price']
+	flight_status = 'upcoming'
+	airplane_id = request.form['airplane_id']
+
+
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO flight VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+					(flight_num, airline_name, arrival_airport_name, depart_airport_name, departure_time, arrival_time, price, flight_status, airplane_id))
+	conn.commit()
+	cursor.close()
+
+	return render_template('viewFlights.html', successfulAdd='Successfully Added Flight!')
 
 app.secret_key = 'its a secret shhhhhhh'
 #Run the app on localhost port 5000
